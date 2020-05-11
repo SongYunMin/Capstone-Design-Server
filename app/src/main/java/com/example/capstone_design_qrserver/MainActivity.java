ca599c509ddc4c9ba216fa99e8d366406327ecd1
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-05-12 02:22:47 +0900

    QRServer App Update

--- app/src/main/java/com/example/capstone_design_qrserver/MainActivity.java
+++ app/src/main/java/com/example/capstone_design_qrserver/MainActivity.java
@@ -2,20 +2,45 @@ package com.example.capstone_design_qrserver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
+    private IntentIntegrator qrScan;
+    public String Local_hash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        qrScan = new IntentIntegrator(this);
+        qrScan.setCameraId(1);                          // 전면카메라 사용
+        qrScan.setOrientationLocked(false);
+        qrScan.initiateScan();
+    }
 
-        new IntentIntegrator(this).initiateScan();
-
-
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+                // todo
+            } else {
+                Local_hash = result.getContents();
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                HttpConnectThread http = new HttpConnectThread(
+                        "http://210.124.110.96/admission.php",
+                        "localhash" + Local_hash);
+                qrScan.initiateScan();              // 재귀적 구현 (Loop 위해)
+                // todo
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
