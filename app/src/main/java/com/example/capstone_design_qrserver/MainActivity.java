73b90df5eb0e19c446f2d6e2fe6b735fea58a624
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-05-14 00:39:26 +0900

    onActivityResult에서 QR인증시 아두이노 결과 전송해야 함

--- app/src/main/java/com/example/capstone_design_qrserver/MainActivity.java
+++ app/src/main/java/com/example/capstone_design_qrserver/MainActivity.java
@@ -2,16 +2,28 @@ package com.example.capstone_design_qrserver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
+import app.akexorcist.bluetotohspp.library.DeviceList;
+
 public class MainActivity extends AppCompatActivity {
     private IntentIntegrator qrScan;
     public String Local_hash;
+    private BluetoothSPP bt = new BluetoothSPP(this);
+    int status = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,34 +37,38 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null) {
+            // 데이터가 없다면
             if (result.getContents() == null) {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
                 // todo
-            } else {
+            }
+            // 데이터가 있다면
+            else {
                 Local_hash = result.getContents();
                 Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
                 HttpConnectThread http = new HttpConnectThread(
                         "http://210.124.110.96/admission.php",
                         "localhash=" + Local_hash);
                 http.start();
+                // 웹서버 결과값 받음
                 String temp = http.GetResult();
                 // 예약이 되어있는 티켓이라면?
-                if(temp.equals("true\n")){
-                    // 티켓 일치시 구현부
+                if (temp.equals("true\n")) { // 티켓 일치시 구현부
+                    status = 1;
                 }
                 // 예약이 되어있는 티켓이 아니라면?
-                else{
+                else {                               // 티켓 불일치시 구현부
 
                 }
-
                 qrScan.initiateScan();              // 재귀적 구현 (Loop 위해)
-                // todo
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
+
 }
+
+
