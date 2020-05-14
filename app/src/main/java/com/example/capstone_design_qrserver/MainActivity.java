a216cc5a803ffcc11b07584c2b98fc694fd683fb
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-05-15 02:11:20 +0900

    아두이노 true 값 전송함

--- app/src/main/java/com/example/capstone_design_qrserver/MainActivity.java
+++ app/src/main/java/com/example/capstone_design_qrserver/MainActivity.java
@@ -1,17 +1,17 @@
 package com.example.capstone_design_qrserver;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.PointF;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.capstone_design_qrserver.HttpConnectThread;
+import com.example.capstone_design_qrserver.R;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -21,24 +21,120 @@ import app.akexorcist.bluetotohspp.library.DeviceList;
 
 public class MainActivity extends AppCompatActivity {
     private IntentIntegrator qrScan;
+    private BluetoothSPP bt;
     public String Local_hash;
-    private BluetoothSPP bt = new BluetoothSPP(this);
-    int status = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt = new BluetoothSPP(this); //Initializing
+
+        if (!bt.isBluetoothAvailable()) { //블루투스 사용 불가
+            Toast.makeText(getApplicationContext()
+                    , "Bluetooth is not available"
+                    , Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() { //데이터 수신
+            public void onDataReceived(byte[] data, String message) {
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bt.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() { //연결됐을 때
+            public void onDeviceConnected(String name, String address) {
+                Toast.makeText(getApplicationContext()
+                        , "Connected to " + name + "\n" + address
+                        , Toast.LENGTH_SHORT).show();
+            }
+
+            public void onDeviceDisconnected() { //연결해제
+                Toast.makeText(getApplicationContext()
+                        , "Connection lost", Toast.LENGTH_SHORT).show();
+            }
+
+            public void onDeviceConnectionFailed() { //연결실패
+                Toast.makeText(getApplicationContext()
+                        , "Unable to connect", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button btnConnect = findViewById(R.id.btnConnect); //연결시도
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (bt.getServiceState() == BluetoothState.STATE_CONNECTED) {
+                    bt.disconnect();
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), DeviceList.class);
+                    startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
+                }
+            }
+        });
+
+        Button QRmod = findViewById(R.id.QRMode);
+        QRmod.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startQR();
+            }
+        });
+    }
+
+    public void startQR() {
         qrScan = new IntentIntegrator(this);
         qrScan.setCameraId(1);                          // 전면카메라 사용
         qrScan.setOrientationLocked(false);
         qrScan.initiateScan();
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onDestroy() {
+        super.onDestroy();
+        bt.stopService(); //블루투스 중지
+    }
+
+    public void onStart() {
+        super.onStart();
+        if (!bt.isBluetoothEnabled()) { //
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent, BluetoothState.REQUEST_ENABLE_BT);
+        } else {
+            if (!bt.isServiceAvailable()) {
+                bt.setupService();
+                bt.startService(BluetoothState.DEVICE_OTHER); //DEVICE_ANDROID는 안드로이드 기기 끼리
+                setup();
+            }
+        }
+    }
+
+    public void setup() {
+        Button btnSend = findViewById(R.id.btnSend); //데이터 전송
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                bt.send("Text", true);
+            }
+        });
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (result != null) {
+        if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
+            if (resultCode == Activity.RESULT_OK)
+                bt.connect(data);
+        } else if (requestCode == BluetoothState.REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_OK) {
+                bt.setupService();
+                bt.startService(BluetoothState.DEVICE_OTHER);
+                setup();
+            } else {
+                Toast.makeText(getApplicationContext()
+                        , "Bluetooth was not enabled."
+                        , Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        } else if (result != null) {
             // 데이터가 없다면
             if (result.getContents() == null) {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
@@ -52,23 +148,23 @@ public class MainActivity extends AppCompatActivity {
                         "http://210.124.110.96/admission.php",
                         "localhash=" + Local_hash);
                 http.start();
+                for (int i = 0; i < 10000; i++) {
+                    System.out.println("qwe");
+                }
+
                 // 웹서버 결과값 받음
                 String temp = http.GetResult();
+
                 // 예약이 되어있는 티켓이라면?
                 if (temp.equals("true\n")) { // 티켓 일치시 구현부
-                    status = 1;
+                    bt.send("1",true);
                 }
                 // 예약이 되어있는 티켓이 아니라면?
-                else {                               // 티켓 불일치시 구현부
-
+                else {
+                    bt.send("0",true);// 티켓 불일치시 구현부
                 }
                 qrScan.initiateScan();              // 재귀적 구현 (Loop 위해)
             }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
         }
     }
-
 }
-
-
